edd17ac57b98e45a54e593c79e4570440ebe26d3
Azhar-lone <azharlonem@gmail.com>
2025-04-18 11:09:20 +0500

    added add user feature in backend

--- apps/api/src/Controllers/user/autherized.js
+++ apps/api/src/Controllers/user/autherized.js
@@ -3,16 +3,91 @@ import { prisma } from '../../libs/connectdb.js';
 // AdminsOnly
 export async function AddUser(req, res) {
   try {
-    const user = await prisma.user.create({
-      data: {
-        email,
-        password,
-        role,
-      },
-    });
   } catch (error) {
     return res.status(500).json({
       msg: 'Internal server error occurred during login',
+    });
+  }
+}
+
+export default async function signUp(req, res) {
+  try {
+    // Destructure the required fields from the request body
+    const { email, password, userName } = req.body;
+
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        OR: [{ email }, { userName }],
+      },
+      select: {
+        email: true,
+        userName: true,
+      },
+    });
+
+    // If either the email or username is taken, handle the response accordingly
+    if (existingUser) {
+      if (existingUser.email === email) {
+        return res.status(403).json({
+          msg: 'Email already registered',
+        });
+      }
+
+      if (existingUser.userName === userName) {
+        const suggestedUserName = await suggestRelatedUsername(
+          userName,
+          (username) =>
+            prisma.user.findUnique({ where: { userName: username } }),
+        );
+        return res.status(403).json({
+          msg: 'Username already taken',
+          suggestedUserName,
+        });
+      }
+    }
+
+    // Hash the password with a salt factor of 5
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        ...(role && { role }),
+      },
+      select: {
+        userName: true,
+        id: true,
+      },
+    });
+
+    // If saving the user fails, return an error response
+    if (!user) {
+      return res.status(500).json({
+        msg: 'Error creating user',
+      });
+    }
+
+    // Create a JWT token for the newly created user
+    const token = createToken(user.id.toString());
+
+    // Send the token as a cookie in the response
+    return res
+      .cookie('login', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+      })
+      .status(200)
+      .json({
+        msg: 'user created successfully',
+        userName: user.userName,
+      });
+  } catch (error) {
+    // Log the error and return a generic error response
+    console.error(error);
+    return res.status(500).json({
+      msg: 'Internal server error',
     });
   }
 }
--- apps/api/src/validators/user.js
+++ apps/api/src/validators/user.js
@@ -1,5 +1,6 @@
-import { check } from 'express-validator';
-
+import { check, body } from 'express-validator';
+import dns from 'dns';
+import { promisify } from 'util';
 export const loginValidation = [
   // email
   check('email')
@@ -12,4 +13,89 @@ export const loginValidation = [
     .withMessage('password is required')
     .trim()
     .escape(),
+];
+
+const allowedDomains = [
+  // 'test.com',
+  'gmail.com',
+  'hotmail.com',
+  'outlook.com',
+  'yahoo.com',
+  'icloud.com',
+  'protonmail.com',
+];
+
+const checkEmailDomain = (email) => {
+  const domain = email.split('@')[1];
+  return allowedDomains.includes(domain);
+};
+const lookupMxRecords = async (domain) => {
+  const resolveMx = promisify(dns.resolveMx);
+  try {
+    const addresses = await resolveMx(domain);
+    return addresses && addresses.length > 0;
+  } catch (err) {
+    return false;
+  }
+};
+
+export const signUpValidation = [
+  // email
+  body('email')
+    .exists()
+    .withMessage('email is required')
+    .isEmail()
+    .withMessage('not a valid email')
+    .custom(async (value) => {
+      if (!checkEmailDomain(value)) {
+        throw new Error('not an allowed domain');
+      }
+      const domain = value.split('@')[1];
+      if (!(await lookupMxRecords(domain))) {
+        throw new Error('email domain does not have valid MX records');
+      }
+      return true;
+    }),
+  
+  // password
+  body('password')
+    .exists()
+    .withMessage('password is required')
+    .trim()
+    .isStrongPassword()
+    .withMessage('not A strong password')
+    .isLength({ max: 16, min: 8 })
+    .withMessage('not a valid length')
+    .escape(),
+
+  // confirm password
+  body('confirmPassword')
+    .exists()
+    .withMessage('confirm password is required')
+    .trim()
+    .escape()
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("password didn't match");
+      }
+      return true;
+    }),
+
+  // user name
+  body('userName')
+    .exists()
+    .withMessage('userName is required')
+    .isString()
+    .withMessage('not a valid string')
+    .isLength({ max: 16 })
+    .withMessage('not a valid length')
+    .isSlug()
+    .withMessage('not a valid userName')
+    .escape(),
+
+  // role
+  body("role")
+    .optional()
+    .isIn(['USER', 'ADMIN', 'SELLER'])
+    .withMessage('Invalid role. Allowed roles are USER, ADMIN, SELLER')
 ];
